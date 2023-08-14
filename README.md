b61c4087daf215cfeb9777bfc818dda5bbea01e5
Jesse-Calebe <62192464+Jesse-Calebe@users.noreply.github.com>
2023-08-13 21:43:53 -0300

    código inicial

--- README.md
+++ README.md
@@ -13,5 +13,8 @@ Configuração inicial
 python -m setupConfigs
 ```
 Após execução do comando acima no prompt vai ser pedido em sequência as seguintes informações:\
+Informações da conta de email de onde vai sair o email\
 Email: <email_configurado_no_serviço_smtp>\
-Senha: <senha_configurada_para_o_email_informado>
+Senha: <senha_configurada_para_o_email_informado>\\
+Informações da conta de email que irá receber o email
+Email: <email>
--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import interfaceAtivos
 import interfaceEmail
 import interfaceExcel
-from log import Log
 
 
 class Orquestrador:
     def __init__(self) -> None:
-        self.log = Log()
-
-        self.log.addLog('Orquestrador.__init__ iniciado')
         self.interfaceAtivos = interfaceAtivos.Ativos()
-        self.log.addLog('interfaceAtivos ok')
         self.interfaceEmail  = interfaceEmail.SOST()
-        self.log.addLog('interfaceEmail ok')
         self.interfaceExcel  = interfaceExcel.GeradorExcel()
-        self.log.addLog('interfaceExcel ok')
         
         self.caminhosArquivos = []
     
     def run(self):
-        self.log.addLog('Orquestrador.run iniciado')
         arquivoFii = self.interfaceExcel.listToExcel(
             'FiiMasterDetail.xlsx',
             self.interfaceAtivos.getFii(),
@@ -29,16 +21,13 @@ class Orquestrador:
         self.caminhosArquivos.append(arquivoFii)
 
         self.enviaEmails()
-        self.log.addLog('Orquestrador.run finalizado')
     
     def enviaEmails(self):
-        self.log.addLog('Orquestrador.enviaEmails iniciado')
         self.interfaceEmail.setSubject('[BOT Python] Extração de Ativos')
         self.interfaceEmail.setBody('Bom dia.\n\nSegue anexo extração de indicadores de ativos.\n\nAtenciosamento,\nPython Bot')
         self.interfaceEmail.setAttachment(self.caminhosArquivos)
 
         self.interfaceEmail.sendEmail()
-        self.log.addLog('Orquestrador.enviaEmails finalizado')
          
          
 orq = Orquestrador()
